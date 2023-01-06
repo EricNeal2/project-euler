f8f4b49f596ab8fa5c30828e274f5b9a6f6f655b
Eric Neal <ericn_4@hotmail.com>
2023-01-06 15:13:45 -0800

    Adding helpers.py and problem_1.ipynb

---
+++ helpers.py
@@ -0,0 +1,10 @@
+def is_multiple(n,m):
+    '''Determines if n is a multiple of m
+    Args:
+        n (int): the larger number
+        m (int): the smaller number
+    Returns:
+        bool
+    '''
+    return n % m == 0
+
